948385a2146aec7e11c6ad6d9e34c78d2f449180
puma13992 <>
2023-06-21 07:17:08 +0000

    Add source code for calculating stock averages

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint 
+from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -27,11 +27,11 @@ def get_sales_data():
         print("Example: 10,20,30,40,50,60\n")
 
         data_str = input("Enter your data here: ")
-    
+
         sales_data = data_str.split(",")
 
         if validate_data(sales_data):
-            print("Valid")
+            print("Data is valid!")
             break
 
     return sales_data
@@ -41,14 +41,14 @@ def validate_data(values):
     """
     Inside the try, converts all string values into integers.
     Raises ValueError if strings cannot be converted into int,
-    or if there aren`t exactly 6 values
+    or if there aren't exactly 6 values.
     """
     try:
         [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
-        )
+            )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
         return False
@@ -58,7 +58,7 @@ def validate_data(values):
 
 def update_worksheet(data, worksheet):
     """
-    Receive a list of integers to be inserted into a worksheet
+    Receives a list of integers to be inserted into a worksheet
     Update the relevant worksheet with the data provided
     """
     print(f"Updating {worksheet} worksheet...\n")
@@ -83,8 +83,8 @@ def calculate_surplus_data(sales_row):
     for stock, sales in zip(stock_row, sales_row):
         surplus = int(stock) - sales
         surplus_data.append(surplus)
-    
-    return surplus_dat
+
+    return surplus_data
 
 
 def get_last_5_entries_sales():
@@ -102,18 +102,36 @@ def get_last_5_entries_sales():
 
     return columns
 
+
+def calculate_stock_data(data):
+    """
+    Calculate the average stock for each item type, adding 10%
+    """
+    print("Calculating stock data...\n")
+    new_stock_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+
+    return new_stock_data
+
+
 def main():
     """
-    Run all program function
+    Run all program functions
     """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
     update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, "surplus")
+    sales_columns = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, "stock")
 
 
-print("Welcome to Love Sandwich Data Automation")
-# main()
-
-sales_columns = get_last_5_entries_sales()
+print("Welcome to Love Sandwiches Data Automation")
+main()
